5895dded58fb68638cf9bea1b91f3c2856d84301
Ricardo Huaripata <ricardohuaripatabellido@gmail.com>
2023-02-16 14:41:59 +0100

    commit

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const GOOGLE_API_KEY = "AIzaSyAKIHCQq4vPTx1PP58dERRwaZe4578t_ZY";
 const RAPID_API_KEY = "0bec2bde07msh72c0629e6e04e01p1892dfjsn084c2fa73404";
 const RAPID_API_HOST = "wft-geo-db.p.rapidapi.com";
 
-var poblaciones = null;
-var latitud = null;
-var longitud = null;
-
 var timeoutId;
 var waitTime = 1000;
 var shouldCallApi = false;
+// Variable donde guardare la lista de poblaciones encontradas
+var poblacionesEncontradas = null;
 
 // Añadimos manejador de eventos onclick a las sugerencias
 for (let i = 0; i < sugerencias.length; i++) {
   sugerencias[i].addEventListener("click", function () {
-    input_ciudad.value = sugerencias[i].textContent;
-    latitud = poblaciones[i].latitude;
-    longitud = poblaciones[i].longitude;
+    iniciarMapa(
+      poblacionesEncontradas[i].latitude,
+      poblacionesEncontradas[i].longitude
+    );
+    document.getElementById("poblacionEncontrada").innerHTML =
+      poblacionesEncontradas[i].name;
+    document.getElementById("comunidad_autonoma").innerHTML =
+      "Comunidad Autonoma: " + poblacionesEncontradas[i].region;
+    document.getElementById("numero_poblacion").innerHTML =
+      "Poblacion: " + poblacionesEncontradas[i].population;
   });
 }
 
@@ -35,7 +40,9 @@ input_ciudad.addEventListener("keyup", function () {
 
       xhr.open(
         "GET",
-        "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=ES&namePrefix=" +
+        "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=" +
+          sugerencias.length +
+          "&countryIds=ES&namePrefix=" +
           input_ciudad.value +
           "&sort=-population&languageCode=es",
         true
@@ -49,17 +56,15 @@ input_ciudad.addEventListener("keyup", function () {
       xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
           var respuesta = JSON.parse(xhr.responseText);
-
+          // controlar que la variable poblacionesEncontradas no se asigne como vacia 
+          // en caso de que la respuesta no contenga ningun elemento 
           if (respuesta.data.length > 0) {
-            poblaciones = respuesta.data;
+            poblacionesEncontradas = respuesta.data;
 
-            for (
-              let i = 0;
-              i < poblaciones.length && i < sugerencias.length;
-              i++
-            ) {
-              sugerencias[i].innerHTML = poblaciones[i].name;
+            for (let i = 0; i < poblacionesEncontradas.length && i < sugerencias.length; i++) {
+              sugerencias[i].innerHTML = poblacionesEncontradas[i].name;
             }
+            
           }
         }
       };
@@ -71,8 +76,48 @@ input_ciudad.addEventListener("keyup", function () {
 
 // Añadimos manejador de eventos onclick al boton de buscar
 document.getElementById("buscar").addEventListener("click", function () {
-  console.log(latitud + ", " + longitud);
-  iniciarMapa(latitud, longitud);
+  if (input_ciudad.value == "") {
+    alert("No has introducido ninguna poblacion");
+  } else {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.open(
+      "GET",
+      "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1" +
+        "&countryIds=ES&namePrefix=" +
+        input_ciudad.value +
+        "&sort=-population&languageCode=es",
+      true
+    );
+
+    xhr.setRequestHeader("X-RapidAPI-Key", RAPID_API_KEY);
+    xhr.setRequestHeader("X-RapidAPI-Host", RAPID_API_HOST);
+
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var respuesta = JSON.parse(xhr.responseText);
+
+        if (respuesta.data.length > 0) {
+          var poblacionEncontrada = respuesta.data[0];
+          iniciarMapa(
+            poblacionEncontrada.latitude,
+            poblacionEncontrada.longitude
+          );
+          document.getElementById("poblacionEncontrada").innerHTML =
+            poblacionEncontrada.name;
+          document.getElementById("comunidad_autonoma").innerHTML =
+            "Comunidad Autonoma: " + poblacionEncontrada.region;
+          document.getElementById("numero_poblacion").innerHTML =
+            "Poblacion: " + poblacionEncontrada.population;
+        } else {
+          alert("No se ha encontrado la poblacion introducida");
+        }
+      }
+    };
+  }
 });
 
 // Funcion para cargar el mapa de google
